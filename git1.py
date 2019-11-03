c7a94b59a02c69a2c4a6ebb9e1b457eacb2b5c66
Ahsan Ehtesham <ahsanehtesham110@gmail.com>
2019-11-03 22:28:45 +0500

    Python Assignment

---
+++ git1.py
@@ -0,0 +1,50 @@
+
+##program1
+
+print("Twinkle, twinkle, little star,")
+print("\t","How I wonder what you are!")
+print("\t","\t","Up above the world so high")
+print("\t","\t","Like a diamond in the sky")
+print("Twinkle, twinkle, little star,")
+print("\t","How I wonder what you are!")
+
+
+
+##program2
+
+import platform
+print(platform.python_version())
+
+
+
+##program3
+
+import datetime
+today=datetime.datetime.now()
+print(today)
+
+
+
+## program4
+
+import math
+radius=int(input('enter the value'))
+area=math.pi*(radius**2)
+print(area)
+
+
+
+##program5
+
+first_name=str(input('enter the first name'))
+last_name=str(input('enter the last name'))
+print(last_name,'',first_name)
+
+
+
+##program6
+a=int(input('enter a first value'))
+b=int(input('enter a second value'))
+addition=a+b
+print('addition', addition)
+
